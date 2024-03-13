a7bd8f7ecacf9fdc6a2617e5a92ae2a38ae59da2
João Rocha <clariosmachine@hotmail.com>
2024-03-13 10:00:39 -0300

    adiciona comentários sobre criação de array

--- arrays.js
+++ arrays.js
@@ -5,7 +5,32 @@
 * Cada elemento pode ser de um tipo diferente;
 * Todo array tem a propriedade length;
 * As Strings se comportam como arrays de caracteres;
+*/
 
+/*
+Um array pode ser criado em sua forma literal apenas indicando que uma variável recebe o valor igual a dois colchetes:
+
+```
+var empty = [];  // Um array sem elementos
+var primes = [2, 3, 5, 7, 11];  // Um array com 5 elementos numéricos
+var misc = [ 1.1, true, "a", ];  // 3 elementos de vários tipos + vírgula à direita
+```
+
+Uma maneira menos usual é criar um array da maneira "explícita". Isso ocorre realizando a atribuição à função construtora de array:
+
+```
+let listaTeste = new Array()  // Cria um array sem elementos
+
+let aFazer = new Array(10)  // cria um array com comprimento especificado, mas sem elementos nem índices. Apenas reserva a qtde de índicese especificada.
+console.log(aFazer[5])
+// -> undefined
+```
+
+Especificação explícita de dois ou mais elementos de array ou de apenas um elemento não numérico para o array:
+ ```
+ var a = new Array(5, 4, 3, 2, 1, "testing, testing");
+ // é a mesma coisa de "var a = [5, 4, 3, 2, 1, 'testing, testing']"
+ ```
 */
 
 let lista1 = ['João', 35, 'casado', 'Patrícia', [1, 2, 3], 1.73]  // Um array com elementos de tipos diferentes
@@ -24,7 +49,14 @@ Quando um array tem várias vírgulas seguidas ele é considerado um array _espa
 Esses valores omitidos são considerados valores _undefined_ ao acessarmos seus índices.
 */
 
+// Array esparso
 const listaEsparsa = [1, 2, , , ,25]
 console.log(listaEsparsa[3], listaEsparsa[0])  // acessando índice que mostrará o valor 'undefined'
-
 // -> undefined 1
+
+console.log(listaEsparsa.length)  // Mostra quantos elementos existem dentro do array. Espaços em branco são contados como elementos undefined.
+// -> 6
+
+var aFazer = new Array(3)
+console.log(aFazer[15])
+// -> undefined
